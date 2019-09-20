648f7b5a78a6bc8886f5972bf65ad9aa92c4ab81
Minh Tuan <minhtuan9922@gmail.com>
2019-09-20 21:17:14 +0700

    remove cache

--- catalog/language/vi-vn/extension/d_blog_module/author.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_['text_error']        = 'Không tìm thấy tác giả!';
-$_['text_review']       = 'bình luận';
-$_['text_views']        = 'lượt xem';
-$_['text_read_more']    = 'đọc thêm ⟶';
-$_['text_tags']       	= 'thẻ:';
-
-$_['text_edit']         = 'Chỉnh sửa';
--- system/config/d_blog_module_layout/grid.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_['d_blog_module_layout'] = array(
-    'id' => 'grid',
-    'name' => 'Grid',
-    'description' => 'Use the layout grid option below to create any kind of layout with rows and columns.',
-    'template' => 'extension/d_blog_module/layout_grid',
-    'styles'=> array(),
-    'scripts' => array()
-);
